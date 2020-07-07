64b64d3b09878eb08b28daee51b164f737f89c7a
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-07-07 11:39:46 -0300

    Register Screen

--- src/screens/Loading/index.js
+++ src/screens/Loading/index.js
@@ -1,16 +1,19 @@
 import React, {useEffect} from 'react';
 import {View, Text, ActivityIndicator} from 'react-native';
+import {useNavigation} from '@react-navigation/stack';
 
 import * as firabase from 'firebase';
 
 import {styles} from './styles';
 
 const Loading = () => {
-  // useEffect(() => {
-  //   firabase.auth().onAuthStateChanged((user) => {
-  //     this.props.navigation.navigate(user ? 'App' : 'Auth');
-  //   });
-  // });
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    firabase.auth().onAuthStateChanged((user) => {
+      navigation.navigate(user ? 'App' : 'Auth');
+    });
+  });
 
   return (
     <View style={styles.container}>
--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,12 +1,77 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import * as firebase from 'firebase';
 
 import {styles} from './styles';
 
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleSignUp = () => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then((userCredentials) => {
+        return userCredentials.user.updateProfile({
+          displayName: this.state.name,
+        });
+      })
+      .catch((error) => this.setState({errorMessage: error.message}));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Register</Text>
+      <Text style={styles.greeting}>{'Hello!\nSign up to get started.'}</Text>
+
+      <View style={styles.errorMessage}>
+        {errorMessage && <Text style={styles.error}>{errorMessage}</Text>}
+      </View>
+      <View style={styles.form}>
+        <View>
+          <Text style={styles.inputTitle}>Full Name</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            onChangeText={(value) => setName(value)}
+            value={name}
+          />
+        </View>
+
+        <View style={{marginTop: 32}}>
+          <Text style={styles.inputTitle}>Email Address</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            onChangeText={(value) => setEmail(value)}
+            value={email}
+          />
+        </View>
+
+        <View style={{marginTop: 32}}>
+          <Text style={styles.inputTitle}>Password</Text>
+          <TextInput
+            style={styles.input}
+            secureTextEntry
+            autoCapitalize="none"
+            onChangeText={(value) => setPassword(value)}
+            value={password}
+          />
+        </View>
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <Text style={{color: '#FFF', fontWeight: '500'}}>Sign up</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{alignSelf: 'center', marginTop: 32}}>
+        <Text style={{color: '#414959', fontSize: 13}}>
+          New to Sociale?{' '}
+          <Text style={{fontWeight: '500', color: '#E9446A'}}>Login</Text>
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
