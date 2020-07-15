fe94cc0a7ae4a4fcb449329637fb586e670eb69a
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-07-14 21:01:02 -0300

    Fix: Loading navigation

--- src/screens/Loading/index.js
+++ src/screens/Loading/index.js
@@ -1,14 +1,11 @@
 import React, {useEffect} from 'react';
 import {View, Text, ActivityIndicator} from 'react-native';
-import {useNavigation} from '@react-navigation/stack';
 
 import * as firabase from 'firebase';
 
 import {styles} from './styles';
 
 const Loading = ({navigation}) => {
-  const navigation = useNavigation();
-
   useEffect(() => {
     firabase.auth().onAuthStateChanged((user) => {
       navigation.navigate(user ? 'App' : 'Auth');
